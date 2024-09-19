7fbb6b2d5081edab70f34cb78c4b5ee4dab143ee
DanielAlmazan <almazansellesdaniel@gmail.com>
2024-09-19 13:59:57 +0200

    Refactor: Removed unnecessary attributes, added screenWidth & refactored card state management
    Removed some unnecessary attributes like `repository: Repository`, `queryParams: [URLQueryItem]` and others.
    
    Added `screenWidth: CGFloat` for measuring the screen via `GeometryReader` and parameterising some hardcoded values. Added `firstMovie: DiscoverMoviesItem?`, `secondMovie: DiscoverMoviesItem?`, `thirdMovie: DiscoverMoviesItem?` `@State private var`s instead of using vm’s `moviesStack: [MovieCardView]?`.
    
    Refactored the rest of the code based on this new perspective.
    
    > The glitch is still present

--- Filmatch/View/Movie/DiscoverMoviesView.swift
+++ Filmatch/View/Movie/DiscoverMoviesView.swift
@@ -9,104 +9,128 @@ import SwiftUI
 
 struct DiscoverMoviesView: View {
   private let maxCardsStack: Int = 3
-  private let repository: Repository
   private let initialSecondCardBlur: Double = 1.5
   private let initialThirdCardBlur: Double = 3
-
   private let acceptBound: Double = 150
   private let declineBound: Double = -150
 
   @State private var vm: DiscoverMoviesViewModel
-  @State private var queryParams: [URLQueryItem]
-  @State private var moviesStack: [MovieCardView?] = []
+
+  @State private var screenWidth: CGFloat?
+
+  @State private var firstMovie: DiscoverMoviesItem?
   @State private var firstCardOffset: CGSize = .zero
-  @State private var firstCardBlurRadius: Double = 0
   @State private var firstCardRotation: Angle = .zero
   @State private var tint: Color = .white
-  @State private var secondCardOffset: CGSize = .zero
+
+  @State private var secondMovie: DiscoverMoviesItem?
   @State private var secondCardBlurRadius: Double = 1.5
   @State private var secondCardRotation: Angle = .degrees(-2)
-  @State private var thirdCardOffset: CGSize = .zero
+
+  @State private var thirdMovie: DiscoverMoviesItem?
   @State private var thirdCardBlurRadius: Double = 3
   @State private var thirdCardRotation: Angle = .degrees(2)
 
   init(repository: Repository) {
-    self.repository = repository
-    vm = DiscoverMoviesViewModel(repository: repository)
-    queryParams = [URLQueryItem(name: "page", value: "1")]
-
+    self.vm = DiscoverMoviesViewModel(repository: repository)
   }
 
   var body: some View {
-    if vm.isLoading {
+    if self.vm.isLoading {
       ProgressView("Loading...")
-    } else if let moviesList = vm.movies {
+    } else if let moviesList = self.vm.movies {
       // MARK: Cards Stack
-      ZStack {
-        if vm.moviesStack.count >= 3, let thirdMovieCard = vm.moviesStack[2] {
-          thirdMovieCard
-            .rotationEffect(thirdCardRotation)
-            .blur(radius: thirdCardBlurRadius)
-            .onAppear {
-              thirdCardRotation = randomRotation(
-                isEven: vm.movies!.count % 2 != 0)
-            }
-        }
-
-        if vm.moviesStack.count >= 2, let secondMovieCard = vm.moviesStack[1] {
-          secondMovieCard
-            .rotationEffect(secondCardRotation)
-            .blur(radius: secondCardBlurRadius)
-            .onAppear {
-              secondCardRotation = randomRotation(
-                isEven: vm.movies!.count % 2 == 0)
-            }
-        }
-
-        if vm.moviesStack.count >= 1, let firstMovieCard = vm.moviesStack[0] {
-          firstMovieCard
-            .offset(firstCardOffset)
-            .rotationEffect(firstCardRotation)
-            .colorMultiply(tint)
-            .gesture(
-              DragGesture()
-                .onChanged { gesture in
-                  let offsetWidth = gesture.translation.width / 0.45
-                  firstCardOffset = .init(width: offsetWidth, height: .zero)
-                  firstCardRotation = .degrees(offsetWidth / 25)
-
-                  withAnimation {
-                    tint = changeColor(width: offsetWidth)
-                  }
+      GeometryReader { geometry in
+        ZStack {
+          if thirdMovie != nil {
+            MovieCardView(movie: $thirdMovie)
+              .rotationEffect(thirdCardRotation)
+              .blur(radius: thirdCardBlurRadius)
+              .onAppear {
+                withAnimation {
+                  thirdCardRotation = randomRotation(
+                    isEven: self.vm.movies!.count % 2 != 0)
                 }
-                .onEnded { _ in
-                  if firstCardOffset.width > acceptBound {
-                    acceptMovie(movie: firstMovieCard.movie!)
-                  } else if firstCardOffset.width < declineBound {
-                    declineMovie(movie: firstMovieCard.movie!)
-                  } else {
+              }
+          }
+
+          if secondMovie != nil {
+            MovieCardView(movie: $secondMovie)
+              .rotationEffect(secondCardRotation)
+              .blur(radius: secondCardBlurRadius)
+              .onAppear {
+                secondCardRotation = randomRotation(
+                  isEven: self.vm.movies!.count % 2 == 0)
+              }
+          }
+
+          if firstMovie != nil {
+            MovieCardView(movie: $firstMovie)
+              .offset(firstCardOffset)
+              .rotationEffect(firstCardRotation)
+              .colorMultiply(tint)
+              .gesture(
+                DragGesture()
+                  .onChanged { gesture in
+                    let offsetWidth = gesture.translation.width / 0.45
+
                     withAnimation {
-                      firstCardOffset = .zero
-                      firstCardRotation = .zero
+                      firstCardOffset = .init(width: offsetWidth, height: .zero)
+                      firstCardRotation = .degrees(offsetWidth / 25)
+                      tint = changeColor(width: offsetWidth)
                     }
                   }
-                }
-            )
-//            .animation(.bouncy, value: firstCardOffset)
+                  .onEnded { _ in
+                    let extraWidth: Double = 70
+
+                    if firstCardOffset.width > acceptBound {
+                      // Movie accepted
+                      acceptMovie(
+                        movie: firstMovie!,
+                        screenWidth: geometry.size.width + extraWidth)
+                    } else if firstCardOffset.width < declineBound {
+                      // Movie declined
+                      declineMovie(
+                        movie: firstMovie!,
+                        screenWidth: geometry.size.width + extraWidth)
+                    } else {
+                      // Nothing happened, reset firstCard's modifiers
+                      withAnimation(.bouncy) {
+                        firstCardOffset = .zero
+                        firstCardRotation = .zero
+                      }
+                    }
+                  }
+              )
+          }
         }
-      }
-      .padding()
-      .onAppear {
-        vm.discoverMovies(
-          withQueryParams: buildQueryParams(page: vm.currentPage))
+        .padding()
+        .onAppear {
+          self.screenWidth = geometry.size.width
+          self.vm.discoverMovies()
+        }
+        .onChange(of: moviesList) { _, newList in
+          if newList.isEmpty { return }
+
+          self.firstMovie = newList.count >= 1 ? newList.first : nil
+          self.secondMovie = newList.count >= 2 ? newList[1] : nil
+          self.thirdMovie = newList.count >= 3 ? newList[2] : nil
+
+          // Fetch more movies if necessary
+          if newList.count == self.maxCardsStack {
+            self.vm.fetchNextPage()
+          }
+        }
       }
 
       // MARK: Buttons
       if let movie = moviesList.first {
-        AcceptDeclineRowButtons(movie: movie, onAccept: acceptMovie, onDecline: declineMovie)
+        AcceptDeclineRowButtons(
+          movie: movie, screenWidth: screenWidth ?? 480, onAccept: acceptMovie,
+          onDecline: declineMovie)
       }
     } else {
-      Text("\(vm.errorMessage ?? "Unexpected error")")
+      Text("\(self.vm.errorMessage ?? "Unexpected error")")
     }
   }
 
@@ -116,71 +140,69 @@ struct DiscoverMoviesView: View {
     return .degrees(isEven ? randomValue : randomValue * -1)
   }
 
-  @MainActor func acceptMovie(movie: DiscoverMoviesItem) {
+  func acceptMovie(movie: DiscoverMoviesItem, screenWidth: CGFloat) {
     print("Movie \(movie.title) accepted")
-    removeCard(moveTo: 450)
+
+    // TODO: Call to server for notifying this user wants to watch this movie
+    removeCard(moveTo: screenWidth)
   }
 
-  @MainActor func declineMovie(movie: DiscoverMoviesItem) {
+  func declineMovie(movie: DiscoverMoviesItem, screenWidth: CGFloat) {
     print("Movie \(movie.title) declined")
-    removeCard(moveTo: -450)
+
+    removeCard(moveTo: -screenWidth)
   }
-  
+
+  /// Handles the process of the first card removal.
+  ///
   /// If the movies list is greater than 3, it will create a newThirdCard before deleting the first one.
   /// That newThirdMovie will be appended to the stack. Then, either the first movie and the first card
   /// will be deleted from each list.
   ///
   /// If the movies list equals 3, it will fetch the next page from the repository.
   ///
-  @MainActor func removeCard(moveTo offset: Double) {
-    withAnimation {
-      // Get the first card away from the view
+  /// - Parameters:
+  ///  - offset: The offset to be moved. It is used for modifying the offset but also for rotating the card.
+  func removeCard(moveTo offset: Double) {
+    guard let movies = vm.movies, !movies.isEmpty else { return }
+
+    let animationDuration = 0.6
+
+    withAnimation(.easeInOut(duration: animationDuration)) {
+      // Move the first card away from the view
       firstCardOffset.width = offset
-      firstCardRotation = .degrees(offset / 50)
-      
-      // Set the second card's rotation and blur to .zero
+      firstCardRotation = .degrees(offset / 30)
+
+      // Set the second card's rotation and blur to zero
       secondCardRotation = .zero
       secondCardBlurRadius = .zero
-      
-      // Reset tint to white
-    } completion: {
-      if vm.movies!.count > 3 {
-        let newThirdCard: MovieCardView = .init(movie: vm.movies![3])
-        vm.moviesStack.append(newThirdCard)
-      }
-      
-      vm.movies?.removeFirst()
-      vm.moviesStack.removeFirst()
-      
-      DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-        firstCardOffset.width = .zero
-        firstCardRotation = .zero
-        secondCardRotation = thirdCardRotation
-        secondCardBlurRadius = initialSecondCardBlur
-        thirdCardRotation = randomRotation(isEven: vm.movies!.count % 2 == 0)
-        tint = .white
-      }
-      
-      if vm.movies!.count == maxCardsStack {
-        vm.currentPage += 1
-        vm.discoverMovies(withQueryParams: buildQueryParams(page: vm.currentPage))
-      }
-      
-    }
-  }
 
-  // TODO: Implement filters
-  /// Gets the filters info and the user's providers for building the query params
-  /// - Returns: An array of URLQueryItem for the URLSession
-  func buildQueryParams(page: Int) -> [URLQueryItem] {
-    [
-      URLQueryItem(name: "page", value: "\(page)"),
-      URLQueryItem(
-        name: "with_watch_providers",
-        value:
-          "\(User.default.providers.map{ "\($0.providerId)" }.joined(separator: ","))"
-      ),
-    ]
+      // Reset tint to white
+      tint = .white
+    } completion: {
+      secondCardRotation = self.thirdCardRotation
+      secondCardBlurRadius = self.initialSecondCardBlur
+
+      if movies.count > 1 {
+        firstMovie = secondMovie
+      }
+
+      if movies.count > 2 {
+        secondMovie = thirdMovie
+        thirdCardRotation = .degrees(offset / 50)
+        thirdCardBlurRadius = self.initialThirdCardBlur
+      }
+
+      // Remove the first movie and card from the lists
+      self.vm.movies?.removeFirst()
+
+      // Reset properties for the next card
+      firstCardOffset = .zero
+      firstCardRotation = .zero
+
+      self.thirdCardRotation = self.randomRotation(
+        isEven: movies.count % 2 == 0)
+    }
   }
 
   func changeColor(width: CGFloat) -> Color {
