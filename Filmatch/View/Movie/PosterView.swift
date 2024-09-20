0302c1f89ed763da6856c10cb9fb4ca8f2e1505c
DanielAlmazan <almazansellesdaniel@gmail.com>
2024-09-20 13:59:03 +0200

    Refactored so that the loaded image and the placeholder will be `maxWidth = .infinity` and `.aspectRatio(2 / 3, contentMode: .fit). Placeholder is now into a ZStack and its progress view has the text "Loading poster..." on it.

--- Filmatch/View/Movie/PosterView.swift
+++ Filmatch/View/Movie/PosterView.swift
@@ -10,29 +10,31 @@ import SwiftUI
 struct PosterView: View {
   let imageUrl: String
   let size: String
-  
+
   var body: some View {
     AsyncImage(url: URL(string: "https://media.themoviedb.org/t/p/\(size)/\(imageUrl)")) { phase in
       switch phase {
         case .failure:
           Image(systemName: "photo")
-            .font(.largeTitle)
-            .aspectRatio(2 / 3, contentMode: .fit)
+            .resizable()
+            .scaledToFit()
+            .padding(40)
         case .success(let image):
           image
             .resizable()
-            .scaledToFit()
-            .aspectRatio(2 / 3, contentMode: .fit)
-            .frame(maxWidth: .infinity)
         default:
-          Color.gray
-          ProgressView()
+          ZStack {
+            Color.gray
+            ProgressView("Loading poster...")
+          }
       }
     }
+    .frame(maxWidth: .infinity)
+    .aspectRatio(2 / 3, contentMode: .fit)
   }
 }
 
-#Preview{
+#Preview {
   PosterView(
     imageUrl: "1m3W6cpgwuIyjtg5nSnPx7yFkXW.jpg",
     size: "original"
