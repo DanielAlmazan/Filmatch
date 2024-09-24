dae98233a025a49027eb37892101cf44f4a52598
DanielAlmazan <almazansellesdaniel@gmail.com>
2024-09-24 12:44:38 +0200

    Added KingFisher and Firebase dependencies.

--- Filmatch/FilmatchApp.swift
+++ Filmatch/FilmatchApp.swift
@@ -5,13 +5,28 @@
 //  Created by Daniel Enrique Almazán Sellés on 23/7/24.
 //
 
+import Firebase
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication
+      .LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct FilmatchApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
