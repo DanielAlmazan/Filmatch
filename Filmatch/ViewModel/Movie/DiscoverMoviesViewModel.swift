6b460c70154ea8658e08f97e5f2d0a2638f956a3
DanielAlmazan <almazansellesdaniel@gmail.com>
2024-10-28 12:55:37 +0100

    Moved DiscoverMoviesListViewModel.swift to ./Movie

--- Filmatch/ViewModel/Movie/DiscoverMoviesViewModel.swift
+++ Filmatch/ViewModel/Movie/DiscoverMoviesViewModel.swift
@@ -7,29 +7,41 @@
 
 import Foundation
 
+/// `DiscoverMoviesViewModel` is responsible for managing the state and data flow for the Discover Movies view.
+/// It interacts with the `MoviesRepository` to fetch movies, handles pagination, loading state, and error messages.
 @Observable
 final class DiscoverMoviesViewModel {
-  private let thirdCardBlur = 3.0
-  private let secondCardBlur = 1.5
-
+  /// The repository used to fetch movie data.
+  let repository: MoviesRepository
+  
+  /// The current page number for pagination.
   var currentPage: Int = 1
+  
+  /// An array of discovered movies.
   var movies: [DiscoverMoviesItem]? = []
+  
+  /// Indicates whether a loading operation is in progress.
   var isLoading: Bool
+  
+  /// An optional error message if an error occurs during data fetching.
   var errorMessage: String?
+  
   // TODO: Add filters
-
-  private let repository: Repository
-
-  init(repository: Repository) {
+  
+  /// Initializes a new instance of `DiscoverMoviesViewModel`.
+  /// - Parameter repository: The `MoviesRepository` used to fetch movie data.
+  init(repository: MoviesRepository) {
     self.repository = repository
     self.isLoading = false
   }
   
+  /// Fetches the next page of movies.
   @MainActor func fetchNextPage() {
     self.currentPage += 1
     discoverMovies()
   }
 
+  /// Fetches movies from the repository based on the current page and updates the `movies` array.
   @MainActor func discoverMovies() {
     self.errorMessage = nil
     self.isLoading = true
@@ -46,13 +58,12 @@ final class DiscoverMoviesViewModel {
         print(self.errorMessage!)
       }
     }
-
     self.isLoading = false
   }
   
   // TODO: Implement filters
-  /// Gets the filters info and the user's providers for building the query params
-  /// - Returns: An array of URLQueryItem for the URLSession
+  /// Builds the query parameters for the API request, including pagination and user's providers.
+  /// - Returns: An array of `URLQueryItem` for the URLSession.
   func buildQueryParams() -> [URLQueryItem] {
     [
       URLQueryItem(name: "page", value: "\(self.currentPage)"),
