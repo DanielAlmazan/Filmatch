6c96446b82842219c30cc3683d46a8b4cde6a84e
DanielAlmazan <almazansellesdaniel@gmail.com>
2024-10-28 13:01:38 +0100

    Added some doc

--- Filmatch/View/Movie/PosterView.swift
+++ Filmatch/View/Movie/PosterView.swift
@@ -8,12 +8,22 @@
 import Kingfisher
 import SwiftUI
 
+/// `PosterView` is a SwiftUI view that displays a movie poster image.
+/// It fetches and displays an image from a given URL, using the Kingfisher library for efficient image loading and caching.
 struct PosterView: View {
+  /// The relative path to the image file.
   let imageUrl: String
+  
+  /// The desired size of the image. This corresponds to the size parameter in the URL.
   let size: String
+  
+  /// The complete URL constructed from the `imageUrl` and `size`.
+  var url: URL {
+    URL(string: "https://media.themoviedb.org/t/p/\(size)/\(imageUrl)")!
+  }
 
   var body: some View {
-    KFImage(URL(string: "https://media.themoviedb.org/t/p/\(size)/\(imageUrl)"))
+    KFImage.url(url)
       .resizable()
       .aspectRatio(2 / 3, contentMode: .fit)
   }
