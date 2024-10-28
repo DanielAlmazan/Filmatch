984812aa7e858fe92a2feb2592394939d341f7a2
DanielAlmazan <almazansellesdaniel@gmail.com>
2024-10-28 13:03:41 +0100

    Improved by using Gauge object

--- Filmatch/View/VotesAverageCircleView.swift
+++ Filmatch/View/VotesAverageCircleView.swift
@@ -1,5 +1,5 @@
 //
-//  AverageVotesCircleView.swift
+//  VotesAverageCircleView.swift
 //  Filmatch
 //
 //  Created by Daniel Enrique Almazán Sellés on 6/9/24.
@@ -9,60 +9,54 @@ import SwiftUI
 
 struct VotesAverageCircleView: View {
   let averageVotes: Double
-  let font: Font
-  var lineWidth: Double
-  var lowAverageRange: ClosedRange<Double>
-  var mediumAverageRange: ClosedRange<Double>
-  var highAverageRange: ClosedRange<Double>
-  
+  var lowAverageRange: Range<Double>
+  var mediumAverageRange: Range<Double>
+  var highAverageRange: Range<Double>
+
   var color: Color {
     switch Double(averageVotes) {
       case lowAverageRange:
         return .red
-        
+
       case mediumAverageRange:
         return .orange
-        
+
       case highAverageRange:
         return .green
-        
+
       default:
         return .gray
     }
   }
-  
+
   init(
     averageVotes: Double,
-    lineWidth: Double = 30,
-    lowAverage: ClosedRange<Double> = 0...0.35,
-    highAverage: ClosedRange<Double> = 0.701...1,
-    font: Font = .footnote
+    lowAverageBound: Double = 0.4,
+    highAverageBound: Double = 0.7
   ) {
     self.averageVotes = averageVotes
-    self.lineWidth = lineWidth
-    self.lowAverageRange = lowAverage
-    self.highAverageRange = highAverage
-    self.mediumAverageRange = lowAverage.upperBound...highAverage.lowerBound
-    self.font = font
+    self.lowAverageRange = 0..<lowAverageBound
+    self.highAverageRange = highAverageBound..<1
+    self.mediumAverageRange = lowAverageBound..<highAverageBound
   }
-  
+
   var body: some View {
-    ZStack {
-      Circle()
-        .stroke(.black.opacity(0.5), lineWidth: self.lineWidth)
-      Circle()
-        .trim(from: 0, to: averageVotes)
-        .stroke(self.color, style: StrokeStyle(lineWidth: self.lineWidth, lineCap: .round))
-        .rotationEffect(.degrees(-90))
-      
-      HStack(spacing: 0) {
-        Text("\(String(format:"%.0f", averageVotes * 100))%")
-          .font(font)
+    Gauge(value: averageVotes, in: 0...1) {
+    } currentValueLabel: {
+      HStack(alignment: .top, spacing: 0) {
+        Text("\(Int(averageVotes * 100))")
+        Text("%", comment: "Percentage for the Average Votes gauge")
+          .font(.caption2)
       }
+      .padding(2)
+      .foregroundStyle(.onBgBase)
     }
+    .gaugeStyle(.accessoryCircularCapacity)
+    .tint(color)
+    .background(.bgBase)
   }
 }
 
 #Preview{
-  VotesAverageCircleView(averageVotes: 0.67)
+  VotesAverageCircleView(averageVotes: 0.7)
 }
