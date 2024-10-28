c56b13f50f5de303b7b37797c9fce9373a02cf8c
DanielAlmazan <almazansellesdaniel@gmail.com>
2024-10-28 13:06:11 +0100

    Implemented HomeView based on login. Added a TabView with Discover, Rooms and Profile tabs.

--- Filmatch/View/HomeView.swift
+++ Filmatch/View/HomeView.swift
@@ -7,30 +7,46 @@
 
 import SwiftUI
 
+/// `HomeView` is the main container view for the Filmatch app, providing a tab-based navigation interface.
+/// It includes tabs for discovering movies, managing rooms, and viewing the user's profile.
+/// This view manages the selection state of the tabs and passes necessary dependencies to child views.
 struct HomeView: View {
-    @State var showingProfile = false
-    
-    var body: some View {
-        NavigationSplitView {
-            List {
-                
-            }
-            .toolbar {
-                Button {
-                    showingProfile.toggle()
-                } label: {
-                    Label("User profile", systemImage: "person.crop.circle")
-                }
-            }
-            .sheet(isPresented: $showingProfile) {
-            }
+  /// The index of the currently selected tab.
+  @State var selectedTab = 0
+  
+  /// The authentication view model used for user authentication and profile management.
+  var authVm: AuthenticationViewModel
+  
+  /// The movies repository used to fetch and provide movie data.
+  var repository: MoviesRepository
 
-        } detail: {
-            Text("Select an option")
-        }
+  var body: some View {
+    TabView(selection: $selectedTab) {
+      // MARK: - Discover Tab
+      /// Tab for discovering movies.
+      Tab("Discover", systemImage: "star", value: 0) {
+        DiscoverMoviesView(repository: repository)
+      }
+
+      // MARK: - Rooms Tab
+      /// Tab for managing and joining rooms.
+      Tab(value: 1) {
+        RoomsMainView()
+      } label: {
+        // Custom label with an image and text for the Rooms tab.
+        Image(.filmatchLogoTabItem)
+        Text("Rooms")
+      }
+
+      // MARK: - Profile Tab
+      /// Tab for viewing and editing the user's profile.
+      Tab("Profile", systemImage: "person.crop.circle", value: 2) {
+        ProfileTab(authVm: authVm)
+      }
     }
+  }
 }
 
 #Preview {
-    HomeView()
+  HomeView(authVm: AuthenticationViewModel(), repository: JsonPresetRepository())
 }
